714237c8061bc0d11e67da5af5139a59b8ca696e
Nicolás Benitez <nicolassbenitez12@gmail.com>
2026-07-22 05:06:27 -0300

    Cambios Esteticos

--- script.js
+++ script.js
@@ -183,3 +183,10 @@ document.querySelectorAll('.galeria-item').forEach(item => {
         };
     });
 });
+
+window.onscroll = function() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("progress-bar").style.width = scrolled + "%";
+};
